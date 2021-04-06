68b359599a7621a61c6029a9109eff77681fa1cb
Vichai1021 <81564011+Vichai1021@users.noreply.github.com>
2021-04-06 22:16:22 +0800

    Add files via upload

---
+++ stairs.js
@@ -0,0 +1,15 @@
+var unit = 7
+
+for (var i=0; i<unit; i++ )
+{  
+for(var j=0; j<i+1; j++)
+document.write("#");
+document.write("<br/>");
+}
+
+for (var i=unit; i>0; i--)
+{
+  for(var j=0; j<i+1; j++)
+  document.write("#")
+  document.write("<br/>")
+}
